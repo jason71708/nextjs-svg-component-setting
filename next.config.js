f007001e342d3e7b77eaad8aae11b78d1c3efbff
Jason Zhuang <jason_zhuang@staging.com.tw>
2022-04-29 15:17:09 +0800

    Complete demo 🎉

--- next.config.js
+++ next.config.js
@@ -1,6 +1,32 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  webpack( config ) {
+    config.module.rules.push( {
+      test: /\.svg$/,
+      issuer: /\.(ts|tsx|js|jsx)$/,
+      use: [
+        {
+          loader: require.resolve('@svgr/webpack'),
+          options: {
+            ref: true,
+          },
+        },
+        {
+          loader: 'next-image-loader',
+          options: {
+            isServer: config.name === 'edge-server' || config.name === 'server',
+            isDev: config.mode === 'development',
+            basePath: "",
+            assetPrefix: "",
+          },
+        },
+      ],
+    });
+
+    return config;
+  }
 }
 
 module.exports = nextConfig
